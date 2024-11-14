125a277861f53c19d023b83501789d3218362e9f
nitin.bhatta0225@gmail.com <nitin.bhatta0225@gmail.com>
2024-11-14 11:32:47 +1100

    "Added initial implementation of simple interest calculator with HTML, CSS, and JavaScript files"

---
+++ app.js
@@ -0,0 +1,10 @@
+let principal = 100;
+let rate = 5;
+let time = 2;
+let interest = 10;
+
+interest = (principal * rate * time) / 100;
+console.log(interest);
+
+let totalamount = interest + principal;
+console.log(totalamount);
